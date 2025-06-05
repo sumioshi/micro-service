e61145cd00b0975a3ec7e230fdcd2f08dfadbcdf
Rodrigo Shodi Sumioshi <shodirodrigo@gmail.com>
2025-06-05 20:24:49 -0300

    fixtures: nextjs erros and install docker for start micro-services

--- apps/book-service/src/app.module.ts
+++ apps/book-service/src/app.module.ts
@@ -6,7 +6,7 @@ import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/book_service_db'),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/book_service_db'),
     BookModule,
   ],
   controllers: [AppController],
--- apps/reservation-service/src/app.module.ts
+++ apps/reservation-service/src/app.module.ts
@@ -6,7 +6,7 @@ import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/reservation_service_db'),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/reservation_service_db'),
     ReservationModule,
   ],
   controllers: [AppController],
